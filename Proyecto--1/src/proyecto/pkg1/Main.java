f54a40d998b584eb3a57dfac27a14ffdfe6aa348
Alessandro Gramcko <107635555+Agramcko@users.noreply.github.com>
2024-10-13 16:39:48 -0400

    Segundo commit

--- Proyecto--1/src/proyecto/pkg1/Main.java
+++ Proyecto--1/src/proyecto/pkg1/Main.java
@@ -8,12 +8,13 @@ package proyecto.pkg1;
  *
  * @author gramc
  */
-public class Proyecto1 {
+public class Main {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("hola");
         // TODO code application logic here
     }
     
