f51b2e42c648476eed2d410e436b9da5c9e132b0
Massimo Gramcko <m.gramcko@correo.unimet.edu.ve>
2024-10-13 18:09:21 -0400

    quinto commit

--- Proyecto--1/src/proyecto/pkg1/Main.java
+++ Proyecto--1/src/proyecto/pkg1/Main.java
@@ -15,6 +15,7 @@ public class Main {
      */
     public static void main(String[] args) {
         System.out.println("hola");
+        System.out.println("listo");
         // TODO code application logic here
     }
     
